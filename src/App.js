211550cb176b039c1ab0a7a3937f5966e0f6cf97
Taowathar <taowathar@gmail.com>
2021-08-25 14:12:30 +0200

    Implement delete function for tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Tasks from './components/Tasks'
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -22,11 +22,21 @@ function App() {
       day: "Feb 5th at 5:30pm",
       reminder: false,
     },
-  ])
+  ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks"
+      )}
     </div>
   );
 }
