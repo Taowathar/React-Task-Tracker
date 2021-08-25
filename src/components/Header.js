9b271b9b1f5a27b448c06b8dc35266af16872264
Taowathar <taowathar@gmail.com>
2021-08-25 13:14:35 +0200

    Add onClick event to the Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,16 @@ import Button from './Button'
 
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
         <header className='header'>
             <h1>{title}</h1>
             {/* <h1 style={headerStyle}>{title}</h1> */}
             {/* <h1 style={{color: 'red', backgroundColor: 'green'}}>{title}</h1> */}
-            <Button color='green' text='Add'></Button>
+            <Button color='green' text='Add' onClick={onClick}></Button>
         </header>
     )
 }
