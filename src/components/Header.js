5d6388ce03dcee4a8f44f22eb0067158cab7850c
Taowathar <taowathar@gmail.com>
2021-08-25 15:24:58 +0200

    Implement Addtask form close

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,32 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('Click')
-    }
-
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            {/* <h1 style={headerStyle}>{title}</h1> */}
-            {/* <h1 style={{color: 'red', backgroundColor: 'green'}}>{title}</h1> */}
-            <Button color='green' text='Add' onClick={onClick}></Button>
-        </header>
-    )
-}
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {/* <h1 style={headerStyle}>{title}</h1> */}
+      {/* <h1 style={{color: 'red', backgroundColor: 'green'}}>{title}</h1> */}
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      ></Button>
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker'
-}
-
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
 // const headerStyle = {
 //     color: 'red',
 //     backgroundColor: 'green'
 // }
 
-export default Header
+export default Header;
